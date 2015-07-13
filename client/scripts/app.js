cf095630e65950cf8f094e8b997d39b69b01e89d
marineAndrea <marine.guillanton@gmail.com>
2015-07-13 16:51:35 -0700

    Displays messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,30 @@
 // YOUR CODE HERE:
 
+// $.get('https://api.parse.com/1/classes/chatterbox');
+
+$(document).ready(function(){
+  var apiURL = 'https://api.parse.com/1/classes/chatterbox';
+  var messages;
+  var text;
+
+  var aj = $.ajax({
+    url: apiURL,
+    type: 'GET',
+    dataType: 'JSON',
+    success: function(data) {
+      messages = data.results;
+      console.log(messages);
+      for (var i = 0; i < messages.length; i++) {
+        if (messages[i].text) {
+          text = messages[i].text;
+          $(".chat").append("<p>" + text + "</p>");
+        }
+      };
+
+      console.log("expect isArray to be true", Array.isArray(messages));
+    },
+    error: function(data) {
+      console.error('Failed to get message');
+    }
+  });
+});
