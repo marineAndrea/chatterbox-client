c611e49158b51c76b43f268e0330ba7130b078c6
marineAndrea <marine.guillanton@gmail.com>
2015-07-14 14:39:49 -0700

    Passes tests through adding rooms to DOM

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,43 +1,12 @@
 var app = {};
-window.initialized = false;
 app.server = 'https://api.parse.com/1/classes/chatterbox';
+app.rooms = [];
 
-app.init = function(room) {
-  // window.roomname;
-  var text;
-  var username;
-  var $newMessage;
-  room = room || "";
-  alert("inside init" + room);
-  // debugger;
+app.init = function(){
+  this.getAll();
+}
 
-  $.ajax({
-    url: this.server,
-    type: 'GET',
-    dataType: 'JSON',
-
-    success: function(data) {
-      var messages = data.results;
-      console.log(messages); //[{}, {} ...]
-      // go through the messages in the appropriate room and display the ones that correspond
-      for (var i = 0; i < messages.length; i++) {
-        if (messages[i].roomname === room) {
-          text = messages[i].text;
-          username = messages[i].username;
-          $newMessage = $('<div></div>'); // why is this working?
-          $newMessage.text(username + ": " + text + ": " + room);
-          $(".chat").append($newMessage);
-        }
-      }
-    },
-    error: function(data) {
-      console.error('Failed to get message');
-    }
-  });
-};
-
-
-app.send = function(newData) {
+app.send = function(newData){
   $.ajax({
     url: this.server,
     type: 'POST',
@@ -47,23 +16,50 @@ app.send = function(newData) {
       alert('chatterbox: Message sent');
     },
     error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       alert('chatterbox: Failed to send message');
     }
   });
-  // refresh each time message is sent
-  alert(newData.roomname);
-  this.fetch(newData.roomname);
   return;
-};
+}
 
-app.fetch = function(room) { 
-  $('.chat').empty();
-  this.init(room);
+app.fetch = function(){
+  this.getAll();
   return;
-};
+}
 
+app.clearMessages = function(){
+  $('#chats').empty();
+  return;
+}
+
+app.addMessage = function(message){
+  text = message.text;
+  username = message.username;
+  $newMessage = $('<div></div>');
+  $newMessage.text(username + ": " + text);
+  $("#chats").append($newMessage);
+  return;
+}
+
+app.addRoom = function(room){
+  $newRoom = $('<option></option>')
+    .attr('value', room)
+    .text(room);
+  $('#roomSelect').append($newRoom);
+  return;
+}
+
+app.addFriend = function() {
+  return;
+}
+
+app.handleSubmit = function() {
+  return;
+}
+
+//EVENT HANDLERS
 $(document).ready(function(){
+  app.init();
   $("#submit").on("click", function() {
     var newData = {};
     newData.username = window.location.search.slice(10); // .split("=")[1]
@@ -71,13 +67,54 @@ $(document).ready(function(){
     newData.roomname = $("#room").val();
     app.send(newData);
   });
-  if (!window.initialized) {
-    alert("called init at bottom" + window.initialized);
-    window.initialized = true;
-    app.init();
-  }
 });
 
+//METHODS
+app.getAll = function(){
+  $.ajax({
+    url: this.server,
+    type: 'GET',
+    dataType: 'JSON',
+    success: function(data) {
+      var messages = data.results;
+      console.log(messages);
+      for (var i = 0; i < messages.length; i++) {
+        app.rooms.push(messages[i].roomname);
+        text = messages[i].text;
+        username = messages[i].username;
+        $newMessage = $('<div></div>');
+        $newMessage.text(username + ": " + text);
+        $("#chats").append($newMessage);
+      }
+    },
+    error: function(data) {
+      console.error('Failed to get message');
+    }
+  });
+}
 
-
-
+/*app.getRoomMessages = function(room) {
+  $.ajax({
+    url: this.server,
+    type: 'GET',
+    dataType: 'JSON',
+    room = room || "";
+    success: function(data) {
+      var messages = data.results;
+      console.log(messages);
+      for (var i = 0; i < messages.length; i++) {
+        if (messages[i].roomname === room) {
+          text = messages[i].text;
+          username = messages[i].username;
+          $newMessage = $('<div></div>');
+          $newMessage.text(username + ": " + text + ": " + room);
+          $("#chats").append($newMessage);
+        }
+      }
+    },
+    error: function(data) {
+      console.error('Failed to get message');
+    }
+  });
+  return;
+}*/
