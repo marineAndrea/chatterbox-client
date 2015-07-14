4449000f7fadfda30d696415c52aad8c3b432e0f
marineAndrea <marine.guillanton@gmail.com>
2015-07-14 10:12:22 -0700

    Added method attr to form

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,67 +1,64 @@
-// YOUR CODE HERE:
+var app = {};
 
-$(document).ready(function(){
-  var app = {};
+app.server = 'https://api.parse.com/1/classes/chatterbox';
 
-  app.server = 'https://api.parse.com/1/classes/chatterbox';
-
-  app.init = function() {
-    $.ajax({
-      url: this.server,
-      type: 'GET',
-      dataType: 'JSON',
-      success: function(data) {
-        var messages = data.results;
-        console.log(messages);
-        var text;
-        var username;
-        var $newMessage;
-        for (var i = 0; i < messages.length; i++) {
-          if (messages[i].text) {
-            text = messages[i].text;
-            username = messages[i].username;
-            $newMessage = $('<div></div>'); // why is this working?
-            $newMessage.text(username + ": " + text);
-            $(".chat").append($newMessage);
-          }
+app.init = function() {
+  $.ajax({
+    url: this.server,
+    type: 'GET',
+    dataType: 'JSON',
+    success: function(data) {
+      var messages = data.results;
+      console.log(messages);
+      var text;
+      var username;
+      var $newMessage;
+      for (var i = 0; i < messages.length; i++) {
+        if (messages[i].text) {
+          text = messages[i].text;
+          username = messages[i].username;
+          $newMessage = $('<div></div>'); // why is this working?
+          $newMessage.text(username + ": " + text);
+          $(".chat").append($newMessage);
         }
-      },
-      error: function(data) {
-        console.error('Failed to get message');
       }
-    });
-  };
+    },
+    error: function(data) {
+      console.error('Failed to get message');
+    }
+  });
+};
 
 
-  app.send = function(newData) {
-    $.ajax({
-      url: this.server,
-      type: 'POST',
-      data: JSON.stringify(newData),
-      contentType: 'application/json',
-      // dataType: 'JSON',
-      success: function (data) {
-        alert('chatterbox: Message sent');
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        alert('chatterbox: Failed to send message');
-      }
-    });
-    return;
-  };
+app.send = function(newData) {
+  $.ajax({
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(newData),
+    contentType: 'application/json',
+    // dataType: 'JSON',
+    success: function (data) {
+      alert('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      alert('chatterbox: Failed to send message');
+    }
+  });
+  return;
+};
 
-  app.fetch = function() { 
-    $('.chat').empty();
-    this.init();
-    return;
-  };
+app.fetch = function() { 
+  $('.chat').empty();
+  this.init();
+  return;
+};
 
-  app.init();
-
+app.init();
+$(document).ready(function(){
   $("#submit").on("click", function() {
     var newData = {};
-    newData.username = window.location.search.slice(10);
+    newData.username = window.location.search.slice(10); // .split("=")[1]
     newData.text = $("#message").val();
     app.send(newData);
   });
