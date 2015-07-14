edf1461216db47e0e71836e49012497bfe6c4532
marineAndrea <marine.guillanton@gmail.com>
2015-07-13 17:20:03 -0700

    Added a button

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,25 +6,32 @@ $(document).ready(function(){
   var apiURL = 'https://api.parse.com/1/classes/chatterbox';
   var messages;
   var text;
-
-  var aj = $.ajax({
-    url: apiURL,
-    type: 'GET',
-    dataType: 'JSON',
-    success: function(data) {
-      messages = data.results;
-      console.log(messages);
-      for (var i = 0; i < messages.length; i++) {
-        if (messages[i].text) {
-          text = messages[i].text;
-          $(".chat").append("<p>" + text + "</p>");
+  var displayMessages = function() {
+    $('.chat').empty();
+    $.ajax({
+      url: apiURL,
+      type: 'GET',
+      dataType: 'JSON',
+      success: function(data) {
+        messages = data.results;
+        console.log(messages);
+        for (var i = 0; i < messages.length; i++) {
+          if (messages[i].text) {
+            text = messages[i].text;
+            $(".chat").append("<p>" + text + "</p>");
+          }
         }
-      };
-
-      console.log("expect isArray to be true", Array.isArray(messages));
-    },
-    error: function(data) {
-      console.error('Failed to get message');
-    }
+      },
+      error: function(data) {
+        console.error('Failed to get message');
+      }
+    });
+  };
+  displayMessages();
+  // debugger;
+  $('.refresh').on('click', function(event){
+    displayMessages();
   });
+
+
 });
